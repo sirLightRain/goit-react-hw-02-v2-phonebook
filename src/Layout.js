66fb04aa82fb17a0f4f010f23389efea0a493a65
Oleksii Stepchuk <34480855+sirLightRain@users.noreply.github.com>
2023-08-30 10:50:51 +0300

    Перші налаштування

---
+++ src/Layout.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+
+  background-color: ${props => props.theme.colors.red};
+`;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+
+
+const theme = {
+  colors: {
+    red: "orangered"
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
